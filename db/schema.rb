2273a504645e86868964d28dde5a4a3460beae5e
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-28 16:23:45 +0600

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -4,14 +4,13 @@
 #
 # This file is the source Rails uses to define your schema when running `bin/rails
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
+# be faster and is potentially less error-prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 20_230_628_094_411) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'comments', force: :cascade do |t|
