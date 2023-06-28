58df7e4edeb8ef81d79803833f192734b024c358
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-28 16:03:53 +0600

    Create migration files with relationships for database

--- db/schema.rb
+++ db/schema.rb
@@ -56,5 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_628_094_411) do
   add_foreign_key 'comments', 'posts'
   add_foreign_key 'comments', 'users', column: 'author_id'
   add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
