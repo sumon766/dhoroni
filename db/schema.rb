64f53a7bc13643be50444e8b87f7b3966c2dbf87
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-28 16:36:32 +0600

    Fix linters errors

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_628_094_411) do
     t.bigint 'author_id'
     t.index ['author_id'], name: 'index_posts_on_author_id'
   end
-end
 
-ActiveRecord::Schema[7.0].define(version: 20_230_628_094_411) do
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'image'
@@ -60,3 +58,4 @@ ActiveRecord::Schema[7.0].define(version: 20_230_628_094_411) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
+
