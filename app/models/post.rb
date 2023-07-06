91519a22defcd62476b582ef3e40e8787613c213
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-07-06 16:08:14 +0600

    Define relationships and add custom methods

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
