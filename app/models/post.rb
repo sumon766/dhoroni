5f01f4413efccd6357cdad5b690c102c9395905f
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-07-14 17:35:51 +0600

    Add post validation tests following the requrements

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  before_save :update_user_posts_counter
-
-  validates :title, presence: { message: 'must be present' },
-                    length: { maximum: 250, message: 'is too long (maximum is 250 characters)' }
+  validates :title, presence: { message: 'must be present' }
+  validate :validate_title_length
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
+  before_save :update_user_posts_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -16,5 +16,11 @@ class Post < ApplicationRecord
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count + 1)
+  end
+
+  def validate_title_length
+    return unless title.present? && title.length > 250
+
+    errors.add(:title, 'is too long (maximum is 250 characters)')
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,4 +20,70 @@ RSpec.describe Post, type: :model do
       expect(association.options[:dependent]).to eq(:destroy)
     end
   end
+
+  let(:author) { User.create(name: 'John Doe') }
+
+  describe 'validations' do
+    it 'is valid with a title within the maximum length' do
+      post = Post.new(title: 'Lorem ipsum dolor sit amet', author: author)
+      expect(post).to be_valid
+    end
+
+    it 'is invalid without a title' do
+      post = Post.new(author: author)
+      expect(post).to be_invalid
+      expect(post.errors[:title]).to include('must be present')
+    end
+
+    it 'is invalid with a title exceeding the maximum length' do
+      post = Post.new(
+        id: 250,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel commodo nisi, sed euismod odio. Proin ultricies tortor nec leo lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel commodo nisi, sed euismod odio. Proin ultri.', # rubocop:disable Layout/LineLength
+        description: 'Test text for the description',
+        comments_counter: 0,
+        likes_counter: 0,
+        author: author,
+        created_at: nil,
+        updated_at: nil
+      )
+      post.validate_title_length
+      post.save
+      expect(post).to be_invalid
+      expect(post.errors[:title]).to include('is too long (maximum is 250 characters)')
+    end
+
+    it 'is valid with a non-negative comments counter' do
+      post = Post.new(title: 'Valid Title', comments_counter: 0, author: author)
+      expect(post).to be_valid
+    end
+
+    it 'is invalid with a negative comments counter' do
+      post = Post.new(comments_counter: -1, author: author)
+      expect(post).to be_invalid
+      expect(post.errors[:comments_counter]).to include('must be greater than or equal to 0')
+    end
+
+    it 'is valid with a non-negative likes counter' do
+      post = Post.new(title: 'Valid Title', likes_counter: 0, author: author)
+      expect(post).to be_valid
+    end
+
+    it 'is invalid with a negative likes counter' do
+      post = Post.new(likes_counter: -1, author: author)
+      expect(post).to be_invalid
+      expect(post.errors[:likes_counter]).to include('must be greater than or equal to 0')
+    end
+  end
+
+  describe '#update_user_posts_counter' do
+    it 'updates the author\'s posts_counter attribute' do
+      author.update(posts_counter: 0)
+
+      expect(author.posts_counter).to eq(0)
+
+      Post.create(title: 'Lorem ipsum', author: author)
+
+      expect(author.reload.posts_counter).to eq(1)
+    end
+  end
 end
