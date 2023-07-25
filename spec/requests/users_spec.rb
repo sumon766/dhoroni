94cc96dc8c7dee30151b33fbcc38ecfd38b69ccf
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-07-26 03:14:11 +0600

    Add integration specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,31 +16,30 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:id' do
     it 'renders the show template' do
       user = User.create(name: 'Alice', bio: 'A test user bio')
-  
+
       user_image_url = 'https://example.com/path/to/valid_image.png'
       user.update(image: user_image_url)
-  
+
       get user_path(user.id)
       expect(response).to have_http_status(200)
       expect(response.body).to include(user.name)
       expect(response.body).to include(user.bio)
-  
+
       expect(response.body).to include(user_image_url)
     end
-  end  
+  end
 
   describe 'GET /users/:user_id' do
     it 'renders the users by id' do
       user_image_url = 'https://example.com/path/to/valid_image.png'
       user = User.create(name: 'Sumon', posts_counter: 2, image: user_image_url)
-      
+
       get "/users/#{user.id}"
-  
+
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
-      
+
       expect(response.body).to include(user_image_url)
     end
   end
-  
 end
