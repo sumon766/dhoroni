e07e102c531716bf040c9c9b7c6bd4ae4e125ac5
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-07-26 00:55:59 +0600

    Integrating the tests for the views

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,18 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /users/:user_id/posts' do
     it 'renders the index template' do
+      user = User.create(id: 5, name: 'Test User')
       get '/users/5/posts'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
-      expect(response.body).to include('List of all posts')
     end
-  end
+  end  
 
   describe 'GET /users/:user_id/posts/:id' do
     it 'renders the show template' do
       user_id = 7
       post_id = 14
-
+  
+      user = User.create(id: user_id, name: 'Test User')
+  
+      post = user.posts.create(id: post_id, title: 'Test Post')
+  
       get "/users/#{user_id}/posts/#{post_id}", params: { user_id: user_id, id: post_id }
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,27 +3,44 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /users' do
     it 'renders the index template' do
-      get '/users'
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:index)
-      expect(response.body).to include('List of all Users')
+      user1 = User.create(name: 'Alice')
+      user2 = User.create(name: 'Bob')
+
+      get users_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
     end
   end
 
-  describe 'GET /users/:user_id' do
-    it 'renders the users by id' do
-      user_id = 7
-      expect { get "/users/#{user_id}" }.to raise_error(ActiveRecord::RecordNotFound)
+  describe 'GET /users/:id' do
+    it 'renders the show template' do
+      user = User.create(name: 'Alice', bio: 'A test user bio')
+  
+      user_image_url = 'https://example.com/path/to/valid_image.png'
+      user.update(image: user_image_url)
+  
+      get user_path(user.id)
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(user.bio)
+  
+      expect(response.body).to include(user_image_url)
     end
-  end
+  end  
 
   describe 'GET /users/:user_id' do
     it 'renders the users by id' do
-      user = User.create(name: 'Sumon', posts_counter: 2)
+      user_image_url = 'https://example.com/path/to/valid_image.png'
+      user = User.create(name: 'Sumon', posts_counter: 2, image: user_image_url)
+      
       get "/users/#{user.id}"
-
+  
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
+      
+      expect(response.body).to include(user_image_url)
     end
   end
+  
 end
