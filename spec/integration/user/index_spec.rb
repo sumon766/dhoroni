fd7ceda3f1c970acecacdac86c587f4c919f9ac0
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-07-31 16:23:41 +0600

    Integration specs in old branch

--- spec/integration/user/index_spec.rb
+++ spec/integration/user/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'User Index', type: :feature do
   before(:each) do
-    @user1 = User.create(name: 'Mehzabien Chowdhury', image: 'https://example.jpg',
+    @user1 = User.create(name: 'Keya Payel', image: 'https://example.jpg',
                          bio: 'She is an actress', posts_counter: 1)
-    @user2 = User.create(name: 'Tasnia Farin', image: 'https://example.jpg',
+    @user2 = User.create(name: 'Tania Bristy', image: 'https://example.jpg',
                          bio: 'She is a student', posts_counter: 90)
     @user3 = User.create(name: 'Zakia Bari Momo', image: 'https://example.jpg',
                          bio: 'She is a writer', posts_counter: 6)
@@ -29,8 +29,8 @@ RSpec.describe 'User Index', type: :feature do
     expect(page.html).to have_content(@user3.posts_counter)
   end
 
-  it 'Redirect to the users show page when clicking on a use.' do
-    click_on @user2.name
+  it 'Redirect to the users show page when clicking on a user.' do
+    click_link @user2.name
     expect(current_path).to eq("/users/#{@user2.id}")
   end
 end
