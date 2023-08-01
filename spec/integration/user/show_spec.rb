17852bf5605f22fc958052d644d6a636583a6e8f
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-08-01 17:09:05 +0600

    Add more integration test as required

--- spec/integration/user/show_spec.rb
+++ spec/integration/user/show_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Show Page', type: :feature do
     @user = User.create(name: 'Tania Brishty', image: 'https://example.jpg',
                         bio: 'She is a software engineer', posts_counter: 10)
     @post1 = Post.create(title: 'test post-1', description: 'test text-1', comments_counter: 1,
-                         likes_counter: 2, author_id: @user.id)
-    @post2 = Post.create(title: 'test post-1', description: 'test text-1', comments_counter: 1,
-                         likes_counter: 2, author_id: @user.id)
-    @post3 = Post.create(title: 'test post-1', description: 'test text-1', comments_counter: 1,
-                         likes_counter: 2, author_id: @user.id)
-    @post4 = Post.create(title: 'test post-1', description: 'test text-1', comments_counter: 1,
-                         likes_counter: 2, author_id: @user.id)
+                         likes_counter: 2, author: @user)
+    @post2 = Post.create(title: 'test post-2', description: 'test text-2', comments_counter: 1,
+                         likes_counter: 2, author: @user)
+    @post3 = Post.create(title: 'test post-3', description: 'test text-3', comments_counter: 1,
+                         likes_counter: 2, author: @user)
+    @post4 = Post.create(title: 'test post-4', description: 'test text-4', comments_counter: 1,
+                         likes_counter: 2, author: @user)
 
     visit user_path(@user)
   end
@@ -48,5 +48,18 @@ RSpec.describe 'Show Page', type: :feature do
       click_link('See all posts')
     end
     expect(current_path).to eq(user_posts_path(@user))
+  end
+
+  it 'Redirects to the post\'s show page when clicking on a user\'s post title' do
+    clicked_post = nil
+    @user.recent_posts.each do |post|
+      if post.title == @post2.title
+        clicked_post = post
+        break
+      end
+    end
+    click_link(clicked_post.title)
+
+    expect(current_path).to eq(user_post_path(@user, clicked_post))
   end
 end
